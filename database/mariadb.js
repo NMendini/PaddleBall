85d231b498d95aa3f3f4b29cd6a5421883741fce
Nicholas Mendini <nicholas.mendini@gmail.com>
2021-01-04 18:13:11 -0600

    Removed initial DB seeding.

--- database/mariadb.js
+++ database/mariadb.js
@@ -27,9 +27,9 @@ connection.query('CREATE TABLE IF NOT EXISTS scores (id INT NOT NULL AUTO_INCREM
   }
 });
 
-connection.query("INSERT INTO scores (initials, score) VALUES('PAD', 50000)");
-connection.query("INSERT INTO scores (initials, score) VALUES('DLE', 30000)");
-connection.query("INSERT INTO scores (initials, score) VALUES('BAL', 10000)");
+// connection.query("INSERT INTO scores (initials, score) VALUES('PAD', 50000)");
+// connection.query("INSERT INTO scores (initials, score) VALUES('DLE', 30000)");
+// connection.query("INSERT INTO scores (initials, score) VALUES('BAL', 10000)");
 
 const getScores = (cb) => {
   connection.query('SELECT * FROM scores', (err, rows) => {
